863ade040605e1ea7691aae4e505c86cebbdc097
Aranyak Samui <aranyaksamuiarya@gmail.com>
2023-04-07 00:15:40 +0530

    Update main.js, change inclustions from concat at check to flexible toggle with a js object and then concat at generation

--- main.js
+++ main.js
@@ -1,75 +1,86 @@
-window.onload = (event) => {
+// Selecting the HTML elements
+const slider = document.querySelector("#char-len-slider");
+const sliderValue = document.querySelector("#slider-value");
+const generateBtn = document.querySelector("#generate-btn");
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numberCheck = document.querySelector("#numbers");
+const symbolsCheck = document.querySelector("#symbols");
+const passwordDisplay = document.querySelector("#password-display");
 
-    // Selecting the HTML elements
-    const slider = document.querySelector("#char-len-slider");
-    const sliderValue = document.querySelector("#slider-value");
-    const generateBtn = document.querySelector("#generate-btn");
-    const uppercaseCheck = document.querySelector("#uppercase");
-    const lowercaseCheck = document.querySelector("#lowercase");
-    const numberCheck = document.querySelector("#numbers");
-    const symbolsCheck = document.querySelector("#symbols");
-    const passwordDisplay = document.querySelector("#password-display");
+// Storing all the characters
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-    // Storing all the characters
-    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const numberChars = "0123456789";
-    const specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+// Updating the slider on sliding
+const generateSliderValue = () => {
+    sliderValue.innerHTML = slider.value;
+    slider.oninput = function () {
+        sliderValue.innerHTML = this.value;
+    };
 
-    
-    let characters = "";
-    let password = "";
+    const currentSliderValue = slider.value;
+    return currentSliderValue;
+};
+generateSliderValue();
 
+// Characters object for inclusions
+let characters = {
+    lowercase: null,
+    uppercase: null,
+    numbers: null,
+    spCharacters: null,
+};
 
-    // Updating the slider on sliding
-    const generateSliderValue = () => {
-        sliderValue.innerHTML = slider.value;
-        slider.oninput = function() {
-            sliderValue.innerHTML = this.value;
-        }
-    
-        const currentSliderValue = Number(sliderValue.innerHTML);
-        return currentSliderValue;
+// Handling inclusions
+uppercaseCheck.addEventListener("input", (e) => {
+    e.target.checked
+    ? (characters = { ...characters, uppercase: uppercaseChars })
+    : (characters = { ...characters, uppercase: null });
+});
+lowercaseCheck.addEventListener("input", (e) => {
+    e.target.checked
+    ? (characters = { ...characters, lowercase: lowercaseChars })
+    : (characters = { ...characters, lowercase: null });
+});
+numberCheck.addEventListener("input", (e) => {
+    e.target.checked
+    ? (characters = { ...characters, numbers: numberChars })
+    : (characters = { ...characters, numbers: null });
+});
+symbolsCheck.addEventListener("input", (e) => {
+    e.target.checked
+    ? (characters = { ...characters, spCharacters: specialChars })
+    : (characters = { ...characters, spCharacters: null });
+});
+
+// Final password output variable
+let password = "";
+
+// Generate password from concatenating the checked inclusions
+const generatePass = () => {
+    let concatString = "";
+    for (const key in characters) {
+        if (characters[key] !== null) concatString += characters[key];
     }
-
-    generateSliderValue();
-   
-
-    uppercaseCheck.onclick = function() {
-        if(uppercaseCheck.checked) {
-            characters += uppercaseChars;
-        }
+    console.log(concatString);
+    for (let i = 0; i < generateSliderValue(); i++) {
+        const randomNo = Math.floor(Math.random() * concatString.length);
+        password += concatString.charAt(randomNo);
     }
+    return password;
+};
 
-    lowercaseCheck.onclick = function() {
-        if(lowercaseCheck.checked) {
-            characters += lowercaseChars;
-        }
-    }
-
-    numberCheck.onclick = function() {
-        if(numberCheck.checked) {
-            characters += numberChars;
-        }
-    }
-
-    symbolsCheck.onclick = function() {
-        if(symbolsCheck.checked) {
-            characters += specialChars;
-        }
-    }
-
-    const generatePass = () => {
-        for (let i = 0; i < generateSliderValue(); i++) {
-            const randomNo = Math.floor(Math.random() * characters.length)
-            password += characters.charAt(randomNo)
-        }
-        return password;
-    }
-    
-
-    generateBtn.addEventListener("click", (event) => {
+// Display the generated password
+generateBtn.addEventListener("click", (event) => {
+    if (!password) {
         passwordDisplay.innerHTML = generatePass();
-    });
-
-}
+        generateBtn.innerHTML = "CLEAR"
+    } else {
+        password = "";
+        passwordDisplay.innerHTML = "P4$5W0RD!";
+        generateBtn.innerHTML = "GENERATE"
+    }
+});
